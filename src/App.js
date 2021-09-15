0c55a12f85e75630c77f8f8569a615ed74815f60
AishwaryaAgr <Aishwarya@23484>
2021-09-15 13:35:21 +0530

    faster

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import QrReader from 'react-qr-reader';
 function App() { 
   const [text, setText] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  const [scanResultFile, setScanResultFile] = useState('');
   const [scanResultWebCam, setScanResultWebCam] =  useState('');
   const classes = useStyles();
   const qrRef = useRef(null);
+  const [done, setDone] = useState(false)
 
 
   const generateQrCode = async () => {
@@ -21,23 +21,13 @@ function App() {
       console.log(error);
     }
   }
-  const handleErrorFile = (error) => {
-    console.log(error);
-  }
-  const handleScanFile = (result) => {
-      if (result) {
-          setScanResultFile(result);
-      }
-  }
-  const onScanFile = () => {
-    qrRef.current.openImageDialog();
-  }
   const handleErrorWebCam = (error) => {
     console.log(error);
   }
   const handleScanWebCam = (result) => {
     if (result){
         setScanResultWebCam(result);
+        console.log(result);
     }
    }
   return (
@@ -59,26 +49,15 @@ function App() {
                               </a>) : null}
                       </Grid>
                       <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                        <Button className={classes.btn} variant="contained" color="secondary" onClick={onScanFile}>Scan Qr Code</Button>
-                        <QrReader
-                          ref={qrRef}
-                          delay={300}
-                          style={{width: '100%'}}
-                          onError={handleErrorFile}
-                          onScan={handleScanFile}
-                          legacyMode
-                        />
-                        <h3>Scanned Code: {scanResultFile}</h3>
-                      </Grid>
-                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
                          <h3>Qr Code Scan by Web Cam</h3>
                          <QrReader
-                         delay={300}
+                         delay={1}
                          style={{width: '100%'}}
                          onError={handleErrorWebCam}
                          onScan={handleScanWebCam}
                          />
-                         <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+                         
+                         <h3>Scanning... {scanResultWebCam}</h3>
                       </Grid>
                   </Grid>
               </CardContent>
